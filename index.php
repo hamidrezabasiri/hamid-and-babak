0938ffb740354e70e88d112b3f6d0273d7a2cf40
babakashtari <ashtaribabak@rocketmail.com>
2020-04-19 00:45:02 +0430

    added a font_imports css page

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/font_imports.css">
     <title>for test</title>
 </head>
 <body>
     <section>
-        <p>for test</p>
+        <p class="iranSans" >این برای تست فونت ایرانسانس</p>
+        <p class="iranSans">این هم برای تست فونت یکان: <span class='Yekan'>123</span></p>
     </section>
 </body>
 </html>
