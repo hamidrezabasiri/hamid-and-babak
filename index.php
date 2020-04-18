3f8c249ddc48cc39064b36966bf09d5983dae88c
babakashtari <ashtaribabak@rocketmail.com>
2020-04-18 23:20:04 +0430

    start of the work

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for test</title>
+</head>
+<body>
+    <section>
+        <p>for test</p>
+    </section>
+</body>
+</html>
